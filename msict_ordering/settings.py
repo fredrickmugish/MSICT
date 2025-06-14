0bc224af64710e20bd8237159e7e98b932e06846
EngOhmz <omarimswaki13@gmail.com>
2025-06-14 16:04:19 +0300

    initial commit

--- msict_ordering/settings.py
+++ msict_ordering/settings.py
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'msict_ordering.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://msict_user:0sG3NcOumz0bE6bSTPm14p9GiNlpj34p@dpg-d16lp015pdvs73ffjvmg-a.oregon-postgres.render.com/msict",
+        conn_max_age=600
+    )
 }
+
+
+
 
 
 # Password validation
